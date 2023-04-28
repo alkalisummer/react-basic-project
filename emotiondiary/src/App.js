2b911eb880ddbb23bd5a19151a28ccb45a2cb7f3
khkang <alkalisummer@gmail.com>
2023-04-28 16:49:38 +0900

    pathVariable, QueryString, pageMoving 적용

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
           <Route path="/diary" element={<Diary />} />
         </Routes>
         <RouteTest />

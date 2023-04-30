6e148d036801c80762ad92f433de231b40e49206
khkang <alkalisummer@gmail.com>
2023-04-30 23:24:18 +0900

    버튼 컴포넌트 생성 및 css 수정

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -4,20 +4,39 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
-
+// Components
+import MyButton from "./components/MyButton";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+        <MyButton
+          text={"버튼"}
+          onClick={() => {
+            alert("버튼 클릭");
+          }}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => {
+            alert("버튼 클릭");
+          }}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => {
+            alert("버튼 클릭");
+          }}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );

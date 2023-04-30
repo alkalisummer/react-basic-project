a09b3397fc1f887e1d3382c7988c9ca5e1cb4211
khkang <alkalisummer@gmail.com>
2023-05-01 01:00:18 +0900

    Header 컴포넌트 추가 및 css 추가

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -4,12 +4,33 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-// Components
+
+// COMPONENTS
 import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽 버튼"}
+              onClick={() => {
+                alert("왼쪽 클릭");
+              }}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => {
+                alert("오른쪽 클릭");
+              }}
+            />
+          }
+        />
         <h2>App.js</h2>
         <MyButton
           text={"버튼"}

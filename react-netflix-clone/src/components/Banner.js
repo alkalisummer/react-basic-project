be8323a2998e55fc3bc95aa9c906b46e4f48c458
khkang <alkalisummer@gmail.com>
2023-05-15 21:15:26 +0900

    the movie api 연동하여 상영중인 영화 data로 banner 영역 구현

--- react-netflix-clone/src/components/Banner.js
+++ react-netflix-clone/src/components/Banner.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import requests from '../api/request';
+import axios from '../api/axios';
+import "./Banner.css"
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();  
+  }, []);
+
+  const fetchData = async() => {
+    
+  // 현재 상영중인 영화정보를 가져오기(여러 영화)
+  const request = await axios.get(requests.fetchNowPlaying);    
+  
+  // 여러 영화중 하나의 영화 ID를 가져오기
+  const movieId = request.data.results[
+    Math.floor(Math.random() * request.data.results.length)
+  ].id;
+
+  // 특정 영화의 상세정보 가져오기(동영상 포함)
+  const {data: movieDetail} = await axios.get(`movie/${movieId}`, {
+    params: {append_to_response : "video"}
+  });
+  
+  setMovie(movieDetail);
+  }
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n-1) + "..." : str  
+  } 
+
   return (
-    <div>Banner</div>
+    <header 
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover"
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>    
+          <button className="banner__button info">More Information</button>
+        </div>
+        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
   )
 }
 
-export default Banner
+export default Banner;

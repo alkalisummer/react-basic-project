82479b327bda5bf9fbe467eebb7a2fdce5dba73d
khkang <alkalisummer@gmail.com>
2023-05-22 13:29:37 +0900

    api 연동하여 검색어에 맞는 영화 목록 데이터 표출

--- react-netflix-clone/src/pages/SearchPage/index.js
+++ react-netflix-clone/src/pages/SearchPage/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from '../../api/axios';
+import "./SearchPage.css";
 
 function SearchPage () {
-  const [serchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);  
   };
@@ -25,10 +26,34 @@ function SearchPage () {
     }
   }
 
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? ( 
+      <section className='search-container'>
+        {searchResults.map((obj)=>{
+          if(obj.backdrop_path !== null && obj.media_type !== 'person'){
+            const movieImageUrl = 'https://image.tmdb.org/t/p/w500' + obj.backdrop_path
+            return (
+              <div className='movie'>
+                <div className='movie__column-poster'>
+                  <img src={movieImageUrl} alt='movie' className='movie__poster'/>
+                </div>
+              </div>
+            )
+          }
+        })}
 
-  return (
-    <div>index</div>
-  )
+      </section>
+    ) : <section className='no-results'>
+          <div className='no-results_text'>
+            <p>
+              찾고자하는 검색어 "{searchTerm}" 에 맞는 영화가 없습니다.
+            </p>
+          </div>       
+        </section>
+  }
+
+
+  return renderSearchResults();
 }
 
 export default SearchPage;
